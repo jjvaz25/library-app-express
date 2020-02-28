bb7115ba552b20a096a2be03915932e2e30d9401
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2020-02-28 14:17:50 -0500

    add database

---
+++ mylibrary/models/author.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, max: 100},
+    family_name: {type: String, required: true, max: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+//Virtual for author's full name
+AuthorSchema
+  .virtual('name')
+  .get(() => {
+    let fullname = '';
+    if (this.first_name && this.family_name) {
+      fullname = `${this.family_name}, ${this.first_name}`;
+    }
+    if (!this.first_name || !this.family_name) {
+      fullname = '';
+    }
+    return fullname;
+  });
+
+//Virtual for author's URL
+AuthorSchema
+  .virtual('url')
+  .get(() => {
+    return `catalog/author${this._id}`;
+  });
+
+module.exports = mongoose.model('Author', AuthorSchema);
